4abdc1fd2c24a05432b8a03c4ca3a173e05c0094
Lucia Diaz <lucia.diaz1498@gmail.com>
2023-08-01 20:55:18 -0300

    M5U4

--- Anime/back/app.js
+++ Anime/back/app.js
@@ -4,9 +4,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+require('dotenv').config();
+var session = require('express-session');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/admin/login')
+var adminRouter = require('./routes/admin/novedades')
+
 var app = express();
 
 // view engine setup
@@ -19,9 +24,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/admin/login', loginRouter);
+app.use('/admin/novedades', adminRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
