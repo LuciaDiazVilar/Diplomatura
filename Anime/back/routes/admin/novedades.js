396ab3eb07aa3bf938245f43f7840b38c3b92990
LuciaDiazVilar <lucia.diaz1498@gmail.com>
2023-08-23 23:32:59 -0300

    update

--- Anime/back/routes/admin/novedades.js
+++ Anime/back/routes/admin/novedades.js
@@ -5,11 +5,42 @@ var novedadesModel = require('./../../models/novedadesModel');
 /* GET home page. */
 router.get('/', async function (req, res, next) {
   const novedades = await novedadesModel.getNovedades();
-    res.render('admin/novedades', {
+  res.render('admin/novedades', {
     layout: 'admin/layout',
     usuario: req.session.nombre,
     novedades
   });
 });
+
+router.get('/agregar', (req, res, next) => {
+  res.render('admin/agregar', {
+    layout: 'admin/layout'
+  })
+})
+
+router.post('/agregar', async (req, res, next) => {
+  try {
+
+    console.log(req.body)
+
+    if (req.body.titulo != "" && req.body.subtitulo != "" && req.body.cuerpo != "") {
+      await novedadesModel.insterNovedad(req.body);
+      res.redirect('/admin/novedades')
+    } else {
+      res.render('admin/agregar', {
+        layout: 'admin/layout',
+        error: true,  
+        message: 'Es necesario completar todos los campos'
+      })
+    }
+  } catch (error) {
+    console.log(error)
+    res.render('admin/agregar', {
+      layout: 'admin/layout',
+      error: true, 
+      message: 'No se pudo cargar la novedad'
+    })
+  }
+})
 
 module.exports = router;
